237da0843da99d087275b0ed9bbee2b658581e97
Vincent Monot <vincent1611@hotmail.fr>
2016-02-22 14:52:45 +0100

    Quartus project

--- quartus/UCPU_16bits.vhd
+++ quartus/UCPU_16bits.vhd
@@ -1,7 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY CPU_16bits IS
+ENTITY UCPU_16bits IS
 PORT (
 	
 	SW : in STD_LOGIC_VECTOR(17 downto 0);
@@ -9,4 +9,9 @@ PORT (
 	LEDG: out STD_LOGIC_VECTOR(7 downto 0);
 	HEX0: out STD_LOGIC_VECTOR(0 to 6)
 );
-END CPU_16bits;
+END UCPU_16bits;
+
+ARCHITECTURE Behavior of UCPU_16bits IS
+BEGIN
+
+END ARCHITECTURE Behavior;
